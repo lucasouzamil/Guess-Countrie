1523065046ba8432d33baa6a693cdb47bd2df8e7
Lucas Lima <lucasfsl@al.insper.edu.br>
2022-05-10 10:17:30 -0300

    mais 3 funcoes

--- JOGO/fcss.py
+++ JOGO/fcss.py
@@ -1,4 +1,5 @@
 import random
+
 def sorteia_letra(palavra,lista):
     listaespecial=['.', ',', '-', ';', ' ']
     x=palavra.lower()
@@ -32,3 +33,43 @@ def adiciona_em_ordem(pais,distancia,lista):
             contador=i+1
     lista.insert(contador,pd)
     return lista
+
+def normaliza(dic_cont_paises):
+    dic_paises_cont = {}
+
+    for continente in dic_cont_paises.keys():
+        for pais in dic_cont_paises[continente].keys():
+            dic_paises_cont[pais] = dic_cont_paises[continente][pais]
+            dic_paises_cont[pais]['continente'] = continente
+    
+    return dic_paises_cont
+
+def sorteia_pais(dic_paises_infos):
+    lista_paises = []
+
+    for pais in dic_paises_infos.keys():
+        lista_paises.append(pais)
+
+    return random.choice(lista_paises)
+
+def haversine(r, phi1,lambda1,phi2,lambda2):
+
+    raio = r #km terra
+
+    phi1graus    = phi1     #latitutede ponto A      [-90,   +90]
+    lambda1graus = lambda1  #longitude ponto A       [−180, +180]
+
+    phi2graus    = phi2     #latitude ponto B        [-90,   +90]
+    lambda2graus = lambda2  #longitude ponto B       [−180, +180]
+
+    phi1rad    = math.radians(phi1graus)       #¦
+    lambda1rad = math.radians(lambda1graus)    #¦  CONVERTE OS DADOS PARA RADIANOS                                            
+    phi2rad    = math.radians(phi2graus)       #¦  PARA UTILIZAR A BIBLIOTECA MATH  
+    lambda2rad = math.radians(lambda2graus)    #¦
+
+    primeiroelemento = (math.sin((phi2rad-phi1rad)/2))**2
+    segundoelemento  = math.cos(phi1rad)math.cos(phi2rad)((math.sin((lambda2rad-lambda1rad)/2))**2)
+
+    d = 2*raio*math.asin((primeiroelemento+segundoelemento)**0.5)
+
+    return d
