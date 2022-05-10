736a5a8c38f90d0bda1c30040fae3cdf970a7a13
Lucas Lima <lucasfsl@al.insper.edu.br>
2022-05-10 11:02:29 -0300

    funcao do print inventario, sorteaia pais

--- JOGO/jogo.py
+++ JOGO/jogo.py
@@ -3,24 +3,34 @@ import math
 import random
 import fcss
 
+
+
 with open('JOGO/DADOS.json', 'r') as data:
     DADOS = data.read()
-
 DADOS = json.loads(DADOS)
-
 DADOS_normalizados = fcss.normaliza(DADOS)
+
+todospaises = []
+for pais in DADOS_normalizados.keys():
+        todospaises.append(pais)
 
 gameon = True
 while gameon:
 
+    inventario = {'distancias':[], 'dicas': []}
+
     tentivas = 20
-    sorteado = random.choice(DADOS_normalizados.keys())
+    sorteado = random.choice(todospaises)
+    print(sorteado)
 
     while tentivas > 0:
 
+        fcss.printinventario(inventario)
         resposta = input('Qual seu palpite?')
 
-        if resposta == paissorteado: #ganhou
+        if resposta == sorteado: #ganhou
+            tentativas = 0
+            gameon = False
 
         #elif resposta in DADOS_normalizados.keys(): #se o pais existe
 
