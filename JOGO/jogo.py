dd92f06c6c081b49197591bf4df349c0f7973c82
Lucas Lima <lucasfsl@al.insper.edu.br>
2022-05-10 11:35:45 -0300

    caso para resposta do jogador ser um pais existente

--- JOGO/jogo.py
+++ JOGO/jogo.py
@@ -14,6 +14,8 @@ todospaises = []
 for pais in DADOS_normalizados.keys():
         todospaises.append(pais)
 
+EARTH_RADIUS = 6371
+
 gameon = True
 while gameon:
 
@@ -22,6 +24,11 @@ while gameon:
     tentivas = 20
     sorteado = random.choice(todospaises)
     print(sorteado)
+
+    latsorteado = DADOS_normalizados[sorteado]['geo']['latitude']
+    longsorteado = DADOS_normalizados[sorteado]['geo']['longitude']
+
+    print(DADOS_normalizados[sorteado])
 
     while tentivas > 0:
 
@@ -33,7 +40,23 @@ while gameon:
             gameon=fcss.jogar_denovo()
             break
         
-        #elif resposta in DADOS_normalizados.keys(): #se o pais existe
+        elif resposta in DADOS_normalizados.keys(): #se o pais existe
+
+            latreposta = DADOS_normalizados[resposta]['geo']['latitude']
+            longreposta = DADOS_normalizados[resposta]['geo']['longitude']
+
+            print(latreposta)
+            print(longreposta)
+
+            distancia = int(fcss.haversine(EARTH_RADIUS, latsorteado,longsorteado,latreposta,longreposta))
+            distancia = float(distancia/1000)
+
+
+            inventario['distancias'].append(f'{distancia} km -> {resposta}')
+
+            tentivas -= 1
+
+
 
         #elif resposta == 'dica':
 
